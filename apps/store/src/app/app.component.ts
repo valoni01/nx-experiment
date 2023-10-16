63b73358520e44fdee9776b7b8fa27c9bf103dcd
Valentine Awe <valentine.awe@gmail.com>
2023-10-16 12:15:53 +0200

    feat: added angular material, and initialise the base application

--- apps/store/src/app/app.component.ts
+++ apps/store/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { getAllGames } from '../fake-api';
 
 @Component({
   selector: 'bg-hoard-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'store';
+  title = 'Board Game Hoard';
+  games = getAllGames();
 }
--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
+    MatCardModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
